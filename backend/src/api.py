964f05928a85b1136312b135583c582061ac9467
James Miller <james.rex.miller.4@gmail.com>
2020-04-16 13:28:03 -0600

    Add functioning delete /drinks/<drink_id> endpoint

--- backend/src/api.py
+++ backend/src/api.py
@@ -124,7 +124,20 @@ def patch_drinks(jwt, drink_id):
     returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks/<int:drink_id>', methods=["DELETE"])
+@requires_auth('delete:drinks')
+def delete_drink(jwt, drink_id):
+    try:
+        drink = Drink.query.filter_by(id=drink_id).one_or_none()
+        drink.delete()
 
+        return jsonify({
+            "success": True,
+            "delete": drink_id
+        })
+
+    except:
+        abort(422)
 
 ## Error Handling
 '''
